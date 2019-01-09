a439debdde26b0068321bcc8f9f205ce65b4eb52
silvio201 <anzengruberf@gmail.com>
2019-01-09 16:35:30 +0100

    Ausarbeiten der Methode Vibrate

--- app/src/main/java/com/example/fanzengruber15/roulettapp/MainActivity.java
+++ app/src/main/java/com/example/fanzengruber15/roulettapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fanzengruber15.roulettapp;
 
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void vibrate(){
-
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
     }
 }
