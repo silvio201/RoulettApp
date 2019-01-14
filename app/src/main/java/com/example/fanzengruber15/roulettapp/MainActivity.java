6ad3f327a49bed28707168adb0276e520237c395
Michael Doppler <mdoppler2000@gmail.com>
2019-01-14 12:00:37 +0100

    Multiplikator

--- app/src/main/java/com/example/fanzengruber15/roulettapp/MainActivity.java
+++ app/src/main/java/com/example/fanzengruber15/roulettapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fanzengruber15.roulettapp;
 
 import android.content.Context;
+import android.graphics.Path;
 import android.icu.text.RelativeDateTimeFormatter;
 import android.os.Build;
 import android.os.VibrationEffect;
@@ -8,7 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.Layout;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Scroller;
 
@@ -25,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         ViewPager slider1=(ViewPager) findViewById(R.id.slider1);
         ViewPager slider2=(ViewPager) findViewById(R.id.slider2);
         ViewPager slider3=(ViewPager) findViewById(R.id.slider3);
-
 
 
         ImageAdapter adapterView1 = new ImageAdapter(this, new int[]{R.mipmap.clubs, R.mipmap.diamonds, R.mipmap.hearts, R.mipmap.spades});
@@ -47,5 +49,37 @@ public class MainActivity extends AppCompatActivity {
             //deprecated in API 26
             v.vibrate(500);
         }
+    }
+
+    public int getMulit(){
+        int multi=0;
+
+        ImageView image1 = null;
+        ImageView image2 = null;
+        ImageView image3 =null;
+
+        ViewPager slider1=(ViewPager) findViewById(R.id.slider1);
+        ViewPager slider2=(ViewPager) findViewById(R.id.slider2);
+        ViewPager slider3=(ViewPager) findViewById(R.id.slider3);
+
+        if(slider1.getCurrentItem() == image1.getId()){
+            multi++;
+        }else{
+            multi--;
+        }
+
+        if(slider2.getCurrentItem() == image2.getId()){
+            multi++;
+        }else{
+            multi--;
+        }
+
+        if(slider3.getCurrentItem() == image3.getId()){
+            multi++;
+        }else{
+            multi--;
+        }
+
+        return multi;
     }
 }
